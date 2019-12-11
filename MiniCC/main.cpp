fd7ec77a3b15cbbee96e2bd4be98b4652f40f0e5
Sergey Karatach <karatach1998@gmail.com>
2019-12-11 23:21:21 +0300

    Implement regex classifier
    It uses DFA to determine the class to which input corresponds.
    The classifier creation process is described by following steps:
      1. The NFA is builded for each regex->class input mapping.
      2. All NFAs are combined to a single NFA.
      3. The DFA is builded from the NFA according to algorithm taken from
         the Dragon Book.

--- MiniCC/main.cpp
+++ MiniCC/main.cpp
@@ -1,17 +1,236 @@
-#include <stddef.h>
 #include <limits.h>
+#include <ctype.h>
+#include <stdlib.h>
 #include <utility>
 #include <array>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
 #include <stack>
 #include <iterator>
+#include <algorithm>
+#include <numeric>
+#include <functional>
 #include <assert.h>
 #include <iostream>
+
+namespace regex { struct State; }
+
+namespace std {
+template <> struct hash<unordered_set<regex::State*>> {
+	size_t operator()(const unordered_set<regex::State*>& set) const;
+};
+}
+
+namespace regex {
+
+
+struct State : std::unordered_multimap<char, State*> {
+	State* add_edge(char sym, State* next) {
+		return std::unordered_multimap<char, State*>::emplace(sym, next)->second;
+	}
+};
+
+struct StateBuffer : std::vector<State> {
+	StateBuffer() : std::vector<State>(1000) {}
+	State* create_state() {
+		std::vector<State>::push_back({});
+		return &std::vector<State>::back();
+	}
+};
+
+struct NFA {
+	State* start_state;
+	// NOTE(sergey): We assume that we create NFAs with a single accepting state.
+	State* accepting_state;
+
+	NFA(StateBuffer& sb) : start_state(sb.create_state()), accepting_state(sb.create_state()) { }
+
+	NFA(State* s, State* a) : start_state(s), accepting_state(a) { }
+
+	NFA(StateBuffer& sb, char sym)
+	{
+		start_state = sb.create_state();
+		accepting_state = sb.create_state();
+
+		start_state->add_edge(sym, accepting_state);
+	}
+
+	void add_transition(char sym)
+	{
+		start_state->add_edge(sym, accepting_state);
+	}
+
+	void add_transitions(char* syms, unsigned n)
+	{
+		for (unsigned i = 0; i < n; ++i)
+			start_state->add_edge(syms[i], accepting_state);
+	}
+
+	NFA union_with(StateBuffer& sb, NFA& other)
+	{
+		auto start = sb.create_state();
+		auto accepting = sb.create_state();
+
+		start->add_edge('\0', start_state);
+		accepting_state->add_edge('\0', accepting);
+		start->add_edge('\0', other.start_state);
+		other.accepting_state->add_edge('\0', accepting);
+
+		return { start, accepting };
+	}
+
+	NFA cat_with(StateBuffer& sb, NFA& other)
+	{
+		accepting_state->add_edge('\0', other.start_state);
+		return { start_state, other.accepting_state };
+	}
+
+	NFA optional(StateBuffer& sb)
+	{
+		auto start = sb.create_state();
+		auto accepting = sb.create_state();
+
+		start->add_edge('\0', start_state);
+		accepting_state->add_edge('\0', accepting);
+		start->add_edge('\0', accepting);
+
+		return { start, accepting };
+	}
+
+	NFA plus(StateBuffer& sb)
+	{
+		auto start = sb.create_state();
+		auto accepting = sb.create_state();
+
+		accepting_state->add_edge('\0', start_state);
+		start->add_edge('\0', start_state);
+		accepting_state->add_edge('\0', accepting);
+
+		return { start, accepting };
+	}
+
+	NFA star(StateBuffer& sb)
+	{
+		auto start = sb.create_state();
+		auto accepting = sb.create_state();
+
+		accepting_state->add_edge('\0', start_state);
+		start->add_edge('\0', start_state);
+		accepting_state->add_edge('\0', accepting);
+		start->add_edge('\0', accepting);
+
+		return { start, accepting };
+	}
+};
+
+struct DFA {
+	std::vector<std::array<short, CHAR_MAX + 1>> transition_table;
+
+	short create_state()
+	{
+		short state_idx = transition_table.size();
+		transition_table.emplace_back();
+		transition_table.back().fill(-1);
+		return state_idx;
+	}
+
+	void add_transition(short origin, short target, char ch)
+	{
+		transition_table[origin][ch] = target;
+	}
+};
+}
+
+size_t std::hash<std::unordered_set<regex::State*>>::operator()(const std::unordered_set<regex::State*>& set) const
+{
+	std::hash<regex::State*> hash;
+	return std::accumulate(std::cbegin(set), std::end(set), size_t(), [&hash](size_t acc, auto x) {
+		return acc ^ hash(x);
+	});
+}
+
+namespace regex {
+auto nfas2dfa(const std::vector<std::pair<NFA, unsigned>> &nfaclses)
+{
+	std::unordered_map<std::unordered_set<State *>, short> states;
+	std::stack<std::pair<std::unordered_set<State *>, short>> states_stack;
+	std::map<State *, unsigned> nfa_state2cls;
+	std::unordered_map<short, unsigned> dfa_state2cls;
+	DFA dfa;
+
+	auto move = [](const std::unordered_set<State *> &states, char ch) {
+		std::unordered_set<State *> res;
+		for (auto &s : states) {
+			auto range = s->equal_range(ch);
+			for (auto it = range.first; it != range.second; ++it)
+				res.emplace(it->second);
+		}
+		return res;
+	};
+
+	auto closure = [](const std::unordered_set<State *> &ss) {
+		std::stack<const State *> stack;
+		std::unordered_set<State *> res{std::begin(ss), std::end(ss)};
+
+		std::for_each(std::begin(ss), std::end(ss), [&stack](auto x) mutable { stack.push(x); });
+		while (!stack.empty()) {
+			auto s = stack.top();
+			stack.pop();
+
+			auto range = s->equal_range('\0');
+			for (auto it = range.first; it != range.second; ++it) {
+				if (res.insert(it->second).second) stack.push(it->second);
+			}
+		}
+		return res;
+	};
+
+	{
+		std::unordered_set<State *> start_states;
+		for (const auto &x : nfaclses) {
+			start_states.emplace(x.first.start_state);
+			nfa_state2cls.emplace(x.first.accepting_state, x.second);
+		}
+		auto p = std::make_pair(closure(start_states), dfa.create_state());
+		states.emplace(p);
+		states_stack.push(p);
+	}
+	while (!states_stack.empty()) {
+		auto t = states_stack.top();
+		states_stack.pop();
+
+		for (int ch = 1; ch <= CHAR_MAX; ++ch) {
+			auto u = closure(move(t.first, ch));
+			if (u.empty()) continue;
+			auto u_it = states.find(u);
+			if (u_it == states.end()) {
+				auto p = std::make_pair(u, dfa.create_state());
+				auto emplace_res = states.insert(p);
+				states_stack.push(p);
+				u_it = emplace_res.first;
+
+				for (auto x : u) {
+					auto state2cls_it = nfa_state2cls.find(x);
+					if (state2cls_it != nfa_state2cls.end()) {
+						dfa_state2cls[u_it->second] = state2cls_it->second;
+						break;
+					}
+				}
+			}
+			dfa.add_transition(t.second, u_it->second, ch);
+		}
+	}
+	return std::make_pair(dfa, dfa_state2cls);
+}
 
 
 struct RegexClassifier {
 	static const unsigned ALPHABET_POWER = CHAR_MAX + 1;
 
-	unsigned* table[ALPHABET_POWER];
+	regex::DFA dfa;
+	std::unordered_map<short, unsigned> state2cls;
 
 	template <typename It>
 	RegexClassifier(It begin, It end)
@@ -22,13 +241,8 @@ struct RegexClassifier {
 	template <typename It>
 	void compile(It begin, It end)
 	{
-		// 
-		// 
-		// 
-		// 
-		// 
-
-		unsigned char state_index = 0;
+		StateBuffer state_buffer;
+		std::vector<std::pair<NFA, unsigned>> nfa4clses;
 
 		for (It it = begin; it != end; ++it)
 		{
@@ -52,129 +266,320 @@ struct RegexClassifier {
 			// SYM -> '\' sym
 			// SYM -> sym
 
-			// --RULES--					--CHOICE--
-			// OR -> CAT OR1				{(,[,\,other,*,+,?}
-			// OR1 -> '|' CAT OR1			{|}
-			// OR1 -> e						{),-|}
-			// CAT -> ASTERIX CAT1			{(,[,\,other,*,+,?}
-			// CAT1 -> ASTERIX CAT1			{(,[,\,other,*,+,?}
-			// CAT1 -> e					{|,),-|}
-			// ASTERIX -> SSET ASTERIX1		{(,[,\,other}
-			// ASTERIX1 -> '*'				{*}
-			// ASTERIX1 -> '+'				{+}
-			// ASTERIX1 -> '?'				{?}
-			// ASTERIX1 -> e				{(,[,\,other,|,),-|}
-			// SSET -> '(' OR ')'			{(}
-			// SSET -> '[' SYMS ']'			{[}
-			// SSET -> SYM					{\,other}
-			// SYMS -> SYM SYMS1			{\,other}
-			// SYMS1 -> SYM SYMS1			{\,other}
-			// SYMS1 -> e					{]}
-			// SYM -> '\' sym				{\}
-			// SYM -> sym					{other}
-			// SYM -> sym '-' sym
+			enum { SYM_STACK_BOTTOM = CHAR_MAX + 1, OR, CAT, ASTERIX, SSET, SYMS, SYM };
+			enum { PANIC, SHIFT, RECOGNIZE };
 
-			enum {OR = 0*CHAR_MAX + 1, OR1, CAT, CAT1, ASTERIX, ASTERIX1, SSET, SYMS, SYMS1, SYM};
+			std::stack<NFA> nfa_stack;
+			std::stack<unsigned> sym_stack;
+			bool inside_brackets = false;
 
-			std::stack<unsigned> stack;
-			stack.push(OR);
+			auto recognize = [&sym_stack, &nfa_stack, &state_buffer, &inside_brackets]() mutable {
+				unsigned m;
+			handle_state0:
+				m = sym_stack.top(); sym_stack.pop();
+				switch (m) {
+				case CAT: goto handle_state1;
+				case ASTERIX: goto handle_state3;
+				case '*': goto handle_state4;
+				case '+': goto handle_state5;
+				case '?': goto handle_state6;
+				case SSET: goto push_asterix;
+				case ')': goto handle_state7;
+				case ']': goto handle_state9;
+				case SYM: goto push_sset;
+				default:
+					sym_stack.push(m);
+					if (inside_brackets) {
+						goto handle_syms;
+					}
+					else {
+						goto handle_sym;
+					}
+				}
+			handle_state1:
+				m = sym_stack.top();
+				switch (m) {
+				case '|': sym_stack.pop(); goto handle_state2;
+				default: goto push_or;
+				}
+			handle_state2:
+				m = sym_stack.top(); sym_stack.pop();
+				switch (m) {
+				case OR: goto handle_or;
+				}
+			handle_state3:
+				m = sym_stack.top();
+				switch (m) {
+				case CAT: sym_stack.pop(); goto handle_cat;
+				default: goto push_cat;
+				}
+			handle_state4:
+				m = sym_stack.top(); sym_stack.pop();
+				switch (m) {
+				case SSET: goto handle_asterix_star;
+				}
+			handle_state5:
+				m = sym_stack.top(); sym_stack.pop();
+				switch (m) {
+				case SSET: goto handle_asterix_plus;
+				}
+			handle_state6:
+				m = sym_stack.top(); sym_stack.pop();
+				switch (m) {
+				case SSET: goto handle_asterix_question;
+				}
+			handle_state7:
+				m = sym_stack.top(); sym_stack.pop();
+				switch (m) {
+				case OR: goto handle_state8;
+				}
+			handle_state8:
+				m = sym_stack.top(); sym_stack.pop();
+				switch (m) {
+				case '(': goto push_sset;
+				}
+			handle_state9:
+				m = sym_stack.top(); sym_stack.pop();
+				switch (m) {
+				case SYMS: goto handle_state10;
+				}
+			handle_state10:
+				m = sym_stack.top(); sym_stack.pop();
+				switch (m) {
+				case '[': goto push_sset;
+				}
+			handle_or: {
+				auto cat_nfa = nfa_stack.top(); nfa_stack.pop();
+				auto or_nfa = nfa_stack.top(); nfa_stack.pop();
+				nfa_stack.push(or_nfa.union_with(state_buffer, cat_nfa));
+				goto push_or;
+			}
+			push_or:
+				sym_stack.push(OR);
+				return;
+			handle_cat: {
+				auto asterix_nfa = nfa_stack.top(); nfa_stack.pop();
+				auto cat_nfa = nfa_stack.top(); nfa_stack.pop();
+				nfa_stack.push(cat_nfa.cat_with(state_buffer, asterix_nfa));
+				goto push_cat;
+			}
+			push_cat:
+				sym_stack.push(CAT);
+				return;
+			handle_asterix_star: {
+				auto sset_nfa = nfa_stack.top(); nfa_stack.pop();
+				nfa_stack.push(sset_nfa.star(state_buffer));
+				goto push_asterix;
+			}
+			handle_asterix_plus: {
+				auto sset_nfa = nfa_stack.top(); nfa_stack.pop();
+				nfa_stack.push(sset_nfa.plus(state_buffer));
+				goto push_asterix;
+			}
+			handle_asterix_question: {
+				auto sset_nfa = nfa_stack.top(); nfa_stack.pop();
+				nfa_stack.push(sset_nfa.optional(state_buffer));
+				goto push_asterix;
+			}
+			push_asterix:
+				sym_stack.push(ASTERIX);
+				return;
+			push_sset:
+				sym_stack.push(SSET);
+				return;
+			handle_syms: {
+				NFA nfa(state_buffer);
+				unsigned sym = sym_stack.top();
+				sym_stack.pop();
 
-			auto replace = [&](const std::initializer_list<unsigned>& seq, bool do_shift = false) mutable {
-				stack.pop();
-				for (auto x : seq)
-					stack.push(x);
-				if (do_shift)
-					++regex_ptr;
+				while (sym_stack.top() != '[') {
+#define FOR_CH(start, end) \
+	for (int ch = (start); ch < (end); ++ch)
+#define FOR_CH_IF(start, end, pred) \
+	FOR_CH(start, end) if (pred(ch))
+					switch (sym_stack.top()) {
+					case '-':
+						sym_stack.pop();
+						FOR_CH(sym_stack.top(), sym + 1) nfa.add_transition(ch);
+						sym = CHAR_MAX + 1;
+						break;
+					case '\\':
+						switch (sym) {
+						case '.': nfa.add_transition('.'); break;
+						case 'd': FOR_CH_IF(0, CHAR_MAX + 1, isdigit) nfa.add_transition(ch); break;
+						case 'D': FOR_CH_IF(0, CHAR_MAX + 1, !isdigit) nfa.add_transition(ch); break;
+						case 's': FOR_CH_IF(0, CHAR_MAX + 1, isspace) nfa.add_transition(ch); break;
+						case 'S': FOR_CH_IF(0, CHAR_MAX + 1, !isspace) nfa.add_transition(ch); break;
+						case 'w': FOR_CH_IF(0, CHAR_MAX + 1, isalpha) nfa.add_transition(ch); nfa.add_transition('_');  break;
+						case 'W': FOR_CH_IF(0, '_', !isalpha) nfa.add_transition(ch); FOR_CH_IF('_' + 1, CHAR_MAX + 1, !isalpha) nfa.add_transition(ch);  break;
+						}
+						sym = CHAR_MAX + 1;
+						break;
+					default:
+						if (sym < CHAR_MAX + 1) {
+							switch (sym) {
+							case '.': FOR_CH(0, CHAR_MAX) nfa.add_transition(ch); break;
+							default: nfa.add_transition(sym); break;
+							}
+						}
+						sym = sym_stack.top();
+					}
+					sym_stack.pop();
+#undef FOR_CH_IF
+#undef FOR_CH
+			}
+			nfa_stack.push(nfa);
+				goto push_syms;
+			}
+			push_syms:
+				sym_stack.push(SYMS);
+				return;
+			handle_sym: {
+				nfa_stack.push({state_buffer, (char)sym_stack.top()});
+				sym_stack.pop();
+				goto push_sym;
+			}
+			push_sym:
+				sym_stack.push(SYM);
+				return;
 			};
 
-			while (!stack.empty()) {
-				auto m = stack.top();
-
+			auto action = [&inside_brackets](unsigned m, char ch) {
 				switch (m) {
-				case OR:
-					switch (*regex_ptr) {
-					case '(': case '[': default: case '*': case '+': case '?': replace({ OR1, CAT }); break;
-					case '\0': stack.pop(); break;
+				case SYM_STACK_BOTTOM:
+					switch (ch) {
+					case '(': case '[': case '\\': default: return SHIFT;
 					}
 					break;
-				case OR1:
-					switch (*regex_ptr) {
-					case '|': replace({ OR1, CAT }, true); break;
-					case ')': case '\0': stack.pop(); break;
+				case OR:
+					switch (ch) {
+					case '|': case ')': return SHIFT;
+					default: return PANIC;
 					}
 					break;
 				case CAT:
-					switch (*regex_ptr) {
-					case '(': case '[': case '\\': default: case '*': case '+': case '?': replace({ CAT1, ASTERIX }); break;
-					}
-					break;
-				case CAT1:
-					switch (*regex_ptr) {
-					case '(': case '[': case '\\': default: case '*': case '+': case '?': replace({ CAT1, ASTERIX }); break;
-					case '|': case ')': case '\0': stack.pop(); break;
+					switch (ch) {
+					case '(': case '[': case '\\': default: return SHIFT;
+					case '|': case ')': case '\0': return RECOGNIZE;
 					}
 					break;
 				case ASTERIX:
-					switch (*regex_ptr) {
-					case '(': case '[': case '\\': default: replace({ ASTERIX1, SSET }); break;
-					}
-					break;
-				case ASTERIX1:
-					switch (*regex_ptr) {
-					case '*': stack.pop(); ++regex_ptr; break;
-					case '+': stack.pop(); ++regex_ptr; break;
-					case '?': stack.pop(); ++regex_ptr; break;
-					case '(': case '[': case '\\': default: case '|': case ')': case '\0': stack.pop(); break;
+					switch (ch) {
+					case '(': case '[': case '\\': default: case '|': case ')': case '\0': return RECOGNIZE;
 					}
 					break;
 				case SSET:
-					switch (*regex_ptr) {
-					case '(': replace({ ')', OR }, true); break;
-					case '[': replace({ ']', SYMS }, true); break;
-					case '\\': default: replace({ SYM }); break;
+					switch (ch) {
+					case '*': case '+': case '?': return SHIFT;
+					case '(': case '[': case '\\': default: case '|': case ')': case '\0': return RECOGNIZE;
 					}
 					break;
 				case SYMS:
-					switch (*regex_ptr) {
-					case '\\': default: replace({ SYMS1, SYM }); break;
-					}
-					break;
-				case SYMS1:
-					switch (*regex_ptr) {
-					case '\\': default: replace({ SYMS1, SYM }); break;
-					case ']': stack.pop(); break;
+					switch (ch) {
+					case ']': return SHIFT;
 					}
 					break;
 				case SYM:
-					stack.pop();
-					if (*regex_ptr == '\\') {
-						++regex_ptr;
+					switch (ch) {
+					case '*': case '+': case '?': case '(': case '[': case '\\': default: case '|': case ')': case '\0': return RECOGNIZE;
 					}
-					++regex_ptr;
-					if (*regex_ptr == '-') {
-						regex_ptr += 2;
+					break;
+				case '|':
+					switch (ch) {
+					case '(': case '[': case '\\': default: return SHIFT;
+					}
+					break;
+				case '*':
+				case '+':
+				case '?':
+					switch (ch) {
+					case '(': case '[': case '\\': default: case '|': case ')': case '\0': return RECOGNIZE;
+					}
+					break;
+				case '(':
+					switch (ch) {
+					case '(': case '[': case '\\': default: return SHIFT;
+					}
+					break;
+				case '[':
+					switch (ch) {
+					default: return SHIFT;
 					}
 					break;
 				case ')':
 				case ']':
-					assert(m == *regex_ptr);
-					stack.pop();
-					++regex_ptr;
+					switch (ch) {
+					case '*': case '+': case '?': case '(': case '[': case '\\': default: case '|': case ')': case '\0': return RECOGNIZE;
+					}
 					break;
+				case '\\':
+				default:
+					if (inside_brackets) {
+						switch (ch) {
+						case ']': return RECOGNIZE;
+						default: return SHIFT;
+						}
+					}
+					else {
+						return RECOGNIZE;
+					}
+				}
+			};
+
+			sym_stack.push(SYM_STACK_BOTTOM);
+			while (true) {
+				auto sym = *regex_ptr;
+				auto m = sym_stack.top();
+				auto a = action(m, sym);
+
+				switch (a) {
+				case PANIC: break;
+				case SHIFT: assert(*regex_ptr); sym_stack.push(*regex_ptr++); break;
+				case RECOGNIZE:
+					recognize();
+					if (sym_stack.top() == OR && *regex_ptr == '\0') {
+						goto next_regex;
+					}
 				}
 
-				printf("%s | ", regex_ptr);
-				auto s = stack;
-				while (!s.empty()) {
-					printf("%d ", s.top());
-					s.pop();
+				switch (sym) {
+				case '[': inside_brackets = true; break;
+				case ']': inside_brackets = false; break;
 				}
-				printf("\n");
 			}
-			printf("-------\n");
+		next_regex:;
+			assert(nfa_stack.size() == 1);
+			nfa4clses.push_back({nfa_stack.top(), (unsigned)cls});
 		}
+
+		auto res = nfas2dfa(nfa4clses);
+		dfa = res.first;
+		state2cls = res.second;
+	}
+
+	auto classify(const char *str) const
+	{
+		short state = 0;
+		int cls_backup = -1;
+		const char *str_backup = str;
+
+		while (*str) {
+			state = dfa.transition_table[state][*str];
+			++str;
+
+			if (state == -1)
+				break;
+
+			auto cls = state2cls.find(state);
+			if (cls != state2cls.cend()) {
+				cls_backup = cls->second;
+			}
+		}
+		return std::make_pair(cls_backup, str_backup);
 	}
 };
+
+}
 
 typedef enum class TokenKind {
 } TokenKind;
@@ -195,9 +600,10 @@ Token nextToken()
 	static std::pair<const char*, unsigned> regex2token_mapping[] = {
 		{"([1-9]b|c)*", 5},
 	};
-	static RegexClassifier regex_classifier(
+	static regex::RegexClassifier regex_classifier(
 		std::cbegin(regex2token_mapping), std::cend(regex2token_mapping));
 
+	printf("#%d\n", regex_classifier.classify("5bc7b"));
 	return { 0 };
 }
 
